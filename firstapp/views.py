893b0fa009c7ec98d86ee6ef8547df60a7145c64
Mexson Fernandes <robomex2020@gmail.com>
2017-06-24 10:13:50 +0530

    Update views.py

--- firstapp/views.py
+++ firstapp/views.py
@@ -3,6 +3,7 @@ from django.core.files.storage import default_storage
 from django.core.files.base import ContentFile, File
 from django.conf import settings
 from django.core.mail import EmailMessage
+from django.core.mail import send_mail
 import os
 import shutil
 from django.http import HttpResponse
@@ -23,10 +24,19 @@ def index(request):
         success = 1
         os.chdir('MEDIA/' + str(email_id) + '/raw_data/')
         os.system('python runAllModels.py')
-        # email = EmailMessage('Model Analysis of dataset | ' + str(csvfile),"Hello Sir/Ma'am\n We have processed your file named as " + str(csvfile) + " and you can check the model analysis done using the same. \n\nTHANK YOU FOR USING OUR SERVICE,\nAlpha Machine" ,from_email=['robomex2020@gmail.com'],to=[str(email_id)])
-        # attach_path = "MEDIA/" + str(email_id) + "/raw_data/finalResult.csv"
-        # email.attach_file('finalResult.csv')
-        # email.send(fail_silently = False)
+#sending mail without attachment
+#         subject = 'Model Analysis of dataset | ' + str(csvfile)
+#         message = "Hello Sir/Ma'am\n We have processed your file named as " + str(csvfile) + " and you can check the model analysis done using the same. \n\nTHANK YOU FOR USING OUR SERVICE,\nAlpha Machine" 
+#         from_email = settings.EMAIL_HOST_USER
+#         to_list = [str(email_id)]
+#         send_mail(subject,message, from_email, to_list, fail_silently = False)
+        try:
+            email = EmailMessage('Model Analysis of dataset | ' + str(csvfile),"body",from_email=['robomex2020@gmail.com'],to=[str(email_id)])
+            attach_path = "MEDIA/" + str(email_id) + "/raw_data/finalResult.csv"
+            email.attach_file('finalResult.csv')
+            email.send(fail_silently = False)
+        except:
+            return HttpResponse(" Error in sending email. Please check your settings.py file.")
     return render(request, "index.html", locals())
 
 def download(request):
