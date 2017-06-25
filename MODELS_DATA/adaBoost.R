c4a6812d12d1fbd0c96884f77b8d3088836f69eb
Mexson Fernandes <robomex2020@gmail.com>
2017-06-25 15:08:55 +0530

    Update adaBoost.R

--- MODELS_DATA/adaBoost.R
+++ MODELS_DATA/adaBoost.R
@@ -1,115 +1,25 @@
-###############################################################
-#                                                             #
-#            AdaBoost for Classification                      #
-#                                                             #
-###############################################################
-#                                                             #
+
 # Credit: Dr. Prashant Singh Rana                             #
-# Email : psrana@gmail.com                                    #
-# Web   : www.psrana.com                                      #
-#                                                             #
-###############################################################
-#                                                             #
-# Train and Test AdaBoost model for Classification            #
-#                                                             #
-# This script do the following:                               #
-# 1. Load the Data                                            #
-# 2. Partition the data into Train/Test set                   #
-# 3. Train the Decision Tree Model                            #
-# 4. Test                                                     #
-# 5. Evaluate on : Accuracy.                                  # 
-# 6. Finally Saving the results.                              #
-#                                                             #
-###############################################################
 
-
-#--------------------------------------------------------------
-# Step 0: Start; Getting the starting time
-#--------------------------------------------------------------
 cat("\nAdaBoost")
 startTime = proc.time()[3]
-
-
-#--------------------------------------------------------------
-# Step 1: Include Library
-#--------------------------------------------------------------
 library(ada)
 library(hmeasure)
-
-#--------------------------------------------------------------
-# Step 2: Variable Declaration
-#--------------------------------------------------------------
 modelName <- "adaBoost"
 InputDataFileName="sample.csv"
 training = 50      # Defining Training Percentage; Testing = 100 - Training
-
-
-#--------------------------------------------------------------
-# Step 3: Data Loading
-#--------------------------------------------------------------
 dataset <- read.csv(InputDataFileName)      # Read the datafile
 dataset <- dataset[sample(nrow(dataset)),]  # Shuffle the data row wise.
-
-
-#--------------------------------------------------------------
-# Step 4: Count total number of observations/rows.
-#--------------------------------------------------------------
 totalDataset <- nrow(dataset)
-
-
-#--------------------------------------------------------------
-# Step 5: Choose Target variable
-#--------------------------------------------------------------
 target  <- names(dataset)[10]   # i.e. Cancer
-
-
-#--------------------------------------------------------------
-# Step 6: Choose inputs Variables
-#--------------------------------------------------------------
 inputs <- setdiff(names(dataset),target)
-
-#Feature Selection
-#n=4
-#inputs <-sample(inputs, n)
-
-
-
-#--------------------------------------------------------------
-# Step 7: Select Training Data Set
-#--------------------------------------------------------------
 trainDataset <- dataset[1:(totalDataset * training/100),c(inputs, target)]
-
-
-#--------------------------------------------------------------
-# Step 8: Select Testing Data Set
-#--------------------------------------------------------------
 testDataset <- dataset[(totalDataset * training/100):totalDataset,c(inputs, target)]
-
-
-#--------------------------------------------------------------
-# Step 9: Model Building (Training)
-#--------------------------------------------------------------
 formula <- as.formula(paste(target, "~", paste(c(inputs), collapse = "+")))
 model <- ada(formula, trainDataset)
-
-#--------------------------------------------------------------
-# Step 10: Prediction (Testing)
-#--------------------------------------------------------------
 Predicted <- predict(model, testDataset)
 PredictedProb <- predict(model, testDataset,type="prob")[,2]
-
-
-#--------------------------------------------------------------
-# Step 11: Extracting Actual
-#--------------------------------------------------------------
 Actual <- as.double(unlist(testDataset[target]))
-
-
-#--------------------------------------------------------------
-# Step 12: Model Evaluation
-#--------------------------------------------------------------
-
-# Step 12.1: Confusion Matrix
 ConfusionMatrix <- misclassCounts(Predicted,Actual)$conf.matrix
 
 
@@ -176,11 +86,6 @@ save.image(file=paste(modelName,"-Model.RData",sep=''))
 
 cat("\nDone")
 cat("\nTotal Time Taken: ", totalTime," sec")
-
-
-#--------------------------------------------------------------
-#                           END 
-#--------------------------------------------------------------
 
 
 
