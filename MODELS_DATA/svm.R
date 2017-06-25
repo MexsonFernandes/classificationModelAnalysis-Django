bddf84546052c58ae0a382257e81b6956a64d53e
Mexson Fernandes <robomex2020@gmail.com>
2017-06-25 15:21:24 +0530

    Update svm.R

--- MODELS_DATA/svm.R
+++ MODELS_DATA/svm.R
@@ -1,27 +1,5 @@
-###############################################################
-#                                                             #
-#            SVM for Classification                           #
-#                                                             #
-###############################################################
-#                                                             #
-# Credit: Dr. Prashant Singh Rana                             #
-# Email : psrana@gmail.com                                    #
-# Web   : www.psrana.com                                      #
-#                                                             #
-###############################################################
-#                                                             #
-# Train and Test SVM model for Classification                 #
-#                                                             #
-# This script do the following:                               #
-# 1. Load the Data                                            #
-# 2. Partition the data into Train/Test set                   #
-# 3. Train the Decision Tree Model                            #
-# 4. Test                                                     #
-# 5. Evaluate on : Accuracy.                                  # 
-# 6. Finally Saving the results.                              #
-#                                                             #
-###############################################################
 
+# Credit: Dr. Prashant Singh Rana                             
 
 #--------------------------------------------------------------
 # Step 0: Start; Getting the starting time
